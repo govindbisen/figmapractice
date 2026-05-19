806099ee629a5322e67f06f935219d0d6b478bd3
unknown <govind.bisen@gmail.com>
2026-05-19 16:21:52 +0530

    first page

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
